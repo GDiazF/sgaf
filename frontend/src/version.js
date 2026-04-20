0dda5b1cc6a6775d9548e0973dadde78fe0a768b
cuillo <cll.dz@hotmail.com>
2026-04-20 17:38:04 -0400

    fix: DATABASE-SYNC-ENV-NAMES (v1.2.2)

--- frontend/src/version.js
+++ frontend/src/version.js
@@ -1,6 +1,6 @@
 // Archivo central de versionamiento del sistema SGAF
 // Estándar SemVer: MAYOR.MENOR.PARCHE
 
-export const APP_VERSION = "1.2.1";
+export const APP_VERSION = "1.2.2";
 export const APP_RELEASE_DATE = "2026-04-20";
 export const APP_DEVELOPER = "Departamento de Servicios Generales, Operaciones y Soporte TI - SLEP IQUIQUE";
